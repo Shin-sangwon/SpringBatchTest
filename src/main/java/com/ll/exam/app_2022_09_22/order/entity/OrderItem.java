b8056a9d568e39990807fb0d084fddd0b01ecb72
Shin-sangwon <dnjsml10@gmail.com>
2022-09-27 09:32:53 +0900

    주문 품목에 판매가, 도매가 기록

--- src/main/java/com/ll/exam/app_2022_09_22/order/entity/OrderItem.java
+++ src/main/java/com/ll/exam/app_2022_09_22/order/entity/OrderItem.java
@@ -29,8 +29,15 @@ public class OrderItem extends BaseEntity {
 
     private int quantity;
 
+    //가격
+    private int price; // 판매가
+    private int salePrice; // 실제 판매가
+    private int wholesalePrice; // 도매가
     public OrderItem(ProductOption productOption, int quantity) {
         this.productOption = productOption;
         this.quantity = quantity;
+        this.price = productOption.getPrice();
+        this.salePrice = productOption.getSalePrice();
+        this.wholesalePrice = productOption.getWholesalePrice();
     }
 }
